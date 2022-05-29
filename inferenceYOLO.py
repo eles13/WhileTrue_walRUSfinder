625bd91136b02a90a77b31c2e56c2a888cf0316d
Rinat Kurbanov <36605450+Demontego@users.noreply.github.com>
2022-05-29 10:16:14 +0300

    fix output data for testing

--- inferenceYOLO.py
+++ inferenceYOLO.py
@@ -17,7 +17,9 @@ if __name__ == '__main__':
     for img in tqdm.tqdm(images):
         out = model(os.path.join(imgdir, img))
         df = out.pandas().xyxy[0]
-        df['x'] = (df['xmax'] + df['xmin']) / 2
-        df['y'] = (df['ymax'] + df['ymin']) / 2
+        df['x'] = (df['xmax'] + df['xmin']) // 2
+        df['y'] = (df['ymax'] + df['ymin']) // 2
+        df['x'] = df['x'].astype(int)
+        df['y'] = df['y'].astype(int)
         df.to_csv(os.path.join(outdir, img[:-3]+'csv'), index=False, columns = ['x', 'y'])
     
